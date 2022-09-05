a23e60790f1ea499020d1e808429d375d8d9d8cb
omerorkn <76444663+omerorkn@users.noreply.github.com>
2022-09-05 23:07:50 +0300

    indentations/spaces edits

--- vending_machine.vhd
+++ vending_machine.vhd
@@ -7,13 +7,14 @@ use IEEE.std_logic_unsigned.all;
 
 entity vending_machine is
 	
-	port( clk 		  : in std_logic ;
-		  rst 		  : in std_logic ;
-		  para_girisi : in std_logic ;
-		  para_miktar : in std_logic_vector(4 downto 0) ;
+	port( 	
+		clk 		: in std_logic ;
+		rst 		: in std_logic ;
+		para_girisi 	: in std_logic ;
+		para_miktar 	: in std_logic_vector(4 downto 0) ;
 		  
-		  para_ustu_hazir   : out std_logic ;
-		  urun_hazir        : out std_logic 
+		para_ustu_hazir : out std_logic ;
+		urun_hazir      : out std_logic 
 
 	    );
 
@@ -27,10 +28,9 @@ signal state : states;
 
 --Process Signal
 
-signal toplam_para : std_logic_vector(4 downto 0);
-signal eksik_para_flag : std_logic;
-
-signal fiyat : std_logic_vector(4 downto 0) := "01010";
+signal toplam_para 	: std_logic_vector(4 downto 0);
+signal eksik_para_flag 	: std_logic;
+signal fiyat 		: std_logic_vector(4 downto 0) := "01010";
 
 begin
 
@@ -40,13 +40,11 @@ begin
 	begin
 
 		if (rst='1') then
-		state 			<= IDLE ;
-		toplam_para 	<= (others => '0');
-		para_ustu_hazir <= '0';
-		urun_hazir      <= '0';
-		eksik_para_flag <= '0';
-		
-		
+			state 		<= IDLE ;
+			toplam_para 	<= (others => '0');
+			para_ustu_hazir <= '0';
+			urun_hazir      <= '0';
+			eksik_para_flag <= '0';
 		elsif(rising_edge(clk)) then
 			case state is
 				when IDLE =>
@@ -56,8 +54,8 @@ begin
 					urun_hazir      <= '0';
 					eksik_para_flag <= '0';
 					if(para_girisi = '1') then
-						state <= PARA_GIRIS ;
-						toplam_para <= toplam_para + para_miktar ;
+						state 		<= PARA_GIRIS ;
+						toplam_para 	<= toplam_para + para_miktar ;
 			
 					else
 						state <= IDLE ;
@@ -65,49 +63,43 @@ begin
 					end if;			
 				when PARA_GIRIS =>
 					if(para_girisi = '1') then
-						state <= PARA_GIRIS ;
-						toplam_para <= toplam_para + para_miktar ;
-					else -- Para giriþi durduysa
+						state 		<= PARA_GIRIS ;
+						toplam_para 	<= toplam_para + para_miktar ;
+					else -- Para giriÃ¾i durduysa
 						if (toplam_para >= fiyat) then
-							state <= URUN_TESLIM ;									
-									
+							state <= URUN_TESLIM ;											
 						else	
 							state <= PARA_USTU ;		
 							eksik_para_flag <= '1';
-							
 						end if;
 					end if;
 
 				
 				when URUN_TESLIM =>
-					toplam_para <= toplam_para - fiyat ;
-					urun_hazir  <= '1' ;
-					state <= PARA_USTU ;
+					toplam_para 	<= toplam_para - fiyat ;
+					urun_hazir  	<= '1' ;
+					state 		<= PARA_USTU ;
 				
 				when PARA_USTU =>
 					eksik_para_flag <= '0' ;
 					if (toplam_para /= "00000") then
-						toplam_para <= toplam_para - "00001" ;
+						toplam_para 	<= toplam_para - "00001" ;
 						para_ustu_hazir <= '1' ;
-						state <= PARA_USTU ;
-						
+						state 		<= PARA_USTU ;
 					else
-					state <= IDLE;
-					para_ustu_hazir <= '0' ;
+						state 		<= IDLE;
+						para_ustu_hazir <= '0' ;
 					
 					end if;
 						
 				when others =>
-					state 			<= IDLE ;
+					state 		<= IDLE ;
 					toplam_para 	<= (others => '0');
 					para_ustu_hazir <= '0';
 					urun_hazir      <= '0';
 					eksik_para_flag <= '0';	
-				
-				
+
 			end case;
-		
 		end if;
-		
 	end process;
 end architecture;
